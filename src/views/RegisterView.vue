a72d831e5e45f5245ca9f0b7c627dcaed176611e
opilane <karin.vetsa@gmail.com>
2025-03-04 23:27:26 +0200

    register view wip

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,13 +1,48 @@
+<template>
+<div>
+  <h1>Loo uus konto</h1>
+  <div class="container">
+    <div class="row mt-5 align-items-center">
+      <div class="col-md-6">
+        <div class="mb-4 d-flex justify-content-end align-items-center">
+          <label class="form-text">Kasutajanimi</label>
+          <input type="text" class="form-control w-auto ms-3" >
+        </div>
+
+        <div class="mb-4 d-flex justify-content-end align-items-center">
+          <label class="form-text">Parool</label>
+          <input type="email" class="form-control w-auto ms-3">
+        </div>
+
+        <div class="mb-4 d-flex justify-content-end align-items-center">
+          <label class="form-text">Parool uuesti</label>
+          <input type="password" class="form-control w-auto ms-3">
+        </div>
+
+        <div class="mb-4 d-flex justify-content-end align-items-center">
+          <label class="form-text">E-mail</label>
+          <input type="email" class="form-control w-auto ms-3">
+        </div>
+      </div>
+
+      <div class="col-md-3 d-flex justify-content-center">
+
+
+        <img src="../assets/profilePictureDefault.webp" height="200" width="200"/></div>
+
+    </div>
+  </div>
+
+</div>
+
+
+</template>
+
+
 <script>
 export default {
   name: "RegisterView"
 }
 </script>
 
-<template>
 
-</template>
-
-<style scoped>
-
-</style>
