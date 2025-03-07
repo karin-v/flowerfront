624b52a1112fa449de1911ddf3cbd54cd96ef2e6
opilane <karin.vetsa@gmail.com>
2025-03-07 15:45:38 +0200

    profile view wip

--- src/views/MyProfileView.vue
+++ src/views/MyProfileView.vue
@@ -1,13 +1,40 @@
+
+<template>
+<div>
+
+  <div class="row justify-content-center">
+    <div class="col">
+      <h2>Minu profiil</h2>
+    </div>
+
+    <div class="row mt-5">
+      <div class="row">
+        <div class="col">
+          <img :src="imageData" class="img-thumbnail" alt="Profiilipilt">
+
+        </div>
+
+        <div class="col">
+          Column
+        </div>
+        </div>
+      <div>
+
+
+      </div>
+    </div>
+
+  </div>
+</div>
+</template>
+
 <script>
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+import AlertSuccess from "@/components/alert/AlertSuccess.vue";
+
 export default {
-  name: "MyProfileView"
+  name: "ProfileView" ,
+  components: {AlertSuccess, AlertDanger}
 }
 </script>
 
-<template>
-
-</template>
-
-<style scoped>
-
-</style>
