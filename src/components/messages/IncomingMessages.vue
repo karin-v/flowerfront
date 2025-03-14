009e5f04ff4c97f7b62686e833d3d68dbb7fbf65
EVelin <evelinmyrk@gmail.com>
2025-03-14 16:29:28 +0200

    14032025 - work: isOwner and closeModal corrected by Rain

--- src/components/messages/IncomingMessages.vue
+++ src/components/messages/IncomingMessages.vue
@@ -1,13 +1,21 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Saatja
+      </div>
+      <div class="col">
+        Pealkiri
+      </div>
+      <div class="col">
+        Timestamp
+      </div>
+    </div>
+  </div>
+</template>
 <script>
 export default {
   name: "IncomingMessages"
 }
 </script>
 
-<template>
-
-</template>
-
-<style scoped>
-
-</style>
