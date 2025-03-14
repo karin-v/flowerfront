a1215f713be5261be1fe829c5b08c48aaaf6175c
opilane <karin.vetsa@gmail.com>
2025-03-14 22:21:40 +0200

    item view kujundus

--- src/components/image/ItemImage.vue
+++ src/components/image/ItemImage.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-  <img v-if="!imageData || imageData.length === 0" src="../../assets/plant.png"  :style="{width: '175px', height: '175px', objectFit: 'cover'}" alt="Default thumbnail">
+  <img v-if="!imageData || imageData.length === 0" src="../../assets/plant.png"  :style="{width: '200px', height: '200px', objectFit: 'cover'}" alt="Default thumbnail">
 
-  <img v-else :src="imageData" :style="{width: '175px', height: '175px', objectFit: 'cover'}" class="img-thumbnail" alt="Taimepilt">
+  <img v-else :src="imageData" :style="{width: '200px', height: '200px', objectFit: 'cover'}" class="img-thumbnail" alt="Taimepilt">
 
 </div>
 </template>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,3 +1,5 @@
+Mall allapoole paigutatud pildiga
+
 <template>
   <div>
     <AlertSuccess :message="successMessage"/>
@@ -19,9 +21,6 @@
                      @event-new-county-selected="setItemCountyId"
                      @event-new-region-selected="setItemRegionId"/>
 
-    <!--                     @event-new-transactiontype-selected="setItemTransactionTypeId"-->
-
-    />
     <NewMessageModal :modal-is-open="newMessageModalIsOpen"
                      @event-update-body="setMessageBody"
                      @event-update-subject="setSubject"
@@ -29,89 +28,198 @@
                      @event-close-modal="closeMessageModal"
     />
     <div class="container mt-4">
-      <div class="row mb-3">
+      <div class="row mb-3 justify-content-center">
+        <div class="col-md-8">
+          <h4 style="color:#212529; font-family: 'Arial', sans-serif; text-align: center;">
+            {{ itemView.transactionType }}: {{ itemView.itemName }}
+          </h4>
 
-        <!--        todo: TransactionType puudu???-->
-        <h4 style="color:#212529; font-family: 'Arial', sans-serif;"> {{ itemView.transactionType }}: {{
-            itemView.itemName
-          }}</h4>
-        <div class="col me-2">
+          <!-- Kitsamad tulbad ja keskendatud layout -->
+          <div class="row justify-content-between">
+            <!-- Vasak tulp - tekst vasakule joondatud -->
+            <div class="col-md-5 d-flex flex-column">
+
+              <div class="mt-5 text-start">
+                <div>Kogus: {{ itemView.totalQuantity }}</div>
+              </div>
+              <div class="mt-1 text-start">
+                <div>Kategooria: {{ itemView.category }}</div>
+              </div>
+              <div class="mt-3">
+                <!-- Kirjeldus õrna raamiga tekstiploki sees - natuke laiem ja kõrgem -->
+                <textarea class="form-control" readonly
+                          style="width: 100%; height: 150px; border: 1px solid #e8e8e8; background: transparent; resize: none; text-align: left; margin-left: 0; padding-left: 0;"
+                          type="text">{{ itemView.description }}</textarea>
+              </div>
 
 
-          <div class="form-floating mt-3">
+              <div class="mt-3 text-start">
+                <div>Kasutaja: {{ itemView.username }}</div>
+              </div>
+              <div class="mt-1 text-start">
+                <div>Asukoht: {{ itemView.county }}, {{ itemView.region }}</div>
+              </div>
+              <div class="mt-1 text-start">
+                Staatus
+              </div>
+              <div class="mt-5" v-if="isOwner">
+                <button @click="openItemInfoModal" type="button" class="btn btn-outline-success me-3">Muuda andmeid</button>
+              </div>
 
-            <p>
-              {{ itemView.description }}
-            </p>
-<!--              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"-->
-<!--                        style="width: 400px; height: 150px; border: none"-->
-<!--                        type="text"-->
-<!--                        :value="itemEdit.description"-->
-<!--                        @input="$emit('event-update-description', $event.target.value)">{{ itemView.description }}</textarea>-->
-          </div>
+              <div v-else>
+                <div class="mt-5">
+                  <button type="button" class="btn btn-success me-3" @click="openMessageModal">Saada kasutajale teade
+                  </button>
+                </div>
+              </div>
+            </div>
 
+            <!-- Parem tulp -->
+            <div class="col-md-5 d-flex flex-column">
+              <!-- Pildi konteiner: suurendatud mt-5 väärtusega, et pilt oleks allpool -->
+              <div class="mt-3 text-center">
+                <!-- Kasutame transform: scale() pildi suurendamiseks ja margin-top pildi allaviimiseks -->
+                <div style="transform: scale(1.5); transform-origin: center top; margin-bottom: 100px; margin-top: 20px;">
+                  <ItemImage :image-data="itemView.itemImage" alt="Taimepilt" />
+                </div>
+              </div>
 
-          <div class="mt-1">
-            <div>Kogus: {{ itemView.totalQuantity }}</div>
-          </div>
-          <div class="mt-1">
-            <div>Kategooria: {{ itemView.category }}</div>
-          </div>
-          <div class="mt-3">
-            <div>Kasutaja: {{ itemView.username }}</div>
-          </div>
-          <div class="mt-1">
-            <div>Asukoht: {{ itemView.county }}, {{ itemView.region }}</div>
-          </div>
-          <div class="mt-1">
-            Staatus
-          </div>
-          <div class="mt-3" v-if="isOwner">
-            <button @click="openItemInfoModal" type="button" class="btn btn-outline-success me-3">Muuda andmeid</button>
+              <!-- Nuppude konteiner piisava vahemikuga pildist -->
+              <div class="mt-3 d-flex flex-column align-items-center">
+<!--                <div class="mb-2">-->
+<!--                  <button type="button" class="btn bg-secondary-subtle">Broneerin</button>-->
+<!--                </div>-->
 
-            <!-- @click="updateItemInfoModal" todo: andmeid saab muuta ja pilti kustutada modalis-->
-            <!-- <ImageInput @event-new-image-selected="$emit('event-new-image-selected', $event)"/>-->
+                <div class="mb-3">
+                  <button @click="navigateToHomeView" type="button" class="btn btn-success">Avalehele</button>
+                </div>
 
-          </div>
-
-          <div v-else>
-            <div class="mt-3">
-              <button type="button" class="btn btn-success me-3" @click="openMessageModal">Saada kasutajale teade
-              </button>
+                <div>
+                  <button type="button" class="btn btn-secondary">Kustuta kuulutus</button>
+                </div>
+              </div>
             </div>
           </div>
-
-        </div>
-        <div class="col">
-          <div class="mt-3">
-
-
-            <ItemImage :image-data="itemView.itemImage" alt="Taimepilt"/>
-
-
-          </div>
-
-          <div class="mt-3">
-            <button type="button" class="btn bg-secondary-subtle me-3">Broneerin</button>
-          </div>
-
-          <div class="mt-3">
-            <button @click="navigateToHomeView" type="button" class="btn btn-success me-3">Avalehele</button>
-          </div>
-          <div>
-            <div class="mt-3">
-              <button type="button" class="btn btn-secondary me-3">Kustuta kuulutus</button>
-              <!--              todo: siia Modal, mis küsib kustutamise kinnitust-->
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <AlertSuccess :message="successMessage"/>-->
+
+<!--    <UpdateItemModal :modal-is-open="updateItemModalIsOpen"-->
+<!--                     :itemEdit="itemEdit"-->
+<!--                     :categories="categories"-->
+<!--                     :counties="counties"-->
+<!--                     :regions="regions"-->
+
+<!--                     @event-close-modal="closeUpdateModal"-->
+<!--                     @event-update-item="updateItem"-->
+
+<!--                     @event-new-image-selected="setImageData"-->
+<!--                     @event-update-item-name="setItemName"-->
+<!--                     @event-update-description="setItemDescription"-->
+<!--                     @event-update-total-quantity="setItemTotalQuantity"-->
+<!--                     @event-new-category-selected="setItemCategoryId"-->
+<!--                     @event-new-county-selected="setItemCountyId"-->
+<!--                     @event-new-region-selected="setItemRegionId"/>-->
+
+<!--    &lt;!&ndash;                     @event-new-transactiontype-selected="setItemTransactionTypeId"&ndash;&gt;-->
+
+<!--    />-->
+<!--    <NewMessageModal :modal-is-open="newMessageModalIsOpen"-->
+<!--                     @event-update-body="setMessageBody"-->
+<!--                     @event-update-subject="setSubject"-->
+<!--                     @event-send-message="sendNewMessage"-->
+<!--                     @event-close-modal="closeMessageModal"-->
+<!--    />-->
+<!--    <div class="container mt-4">-->
+<!--      <div class="row mb-3">-->
+
+<!--        &lt;!&ndash;        todo: TransactionType puudu???&ndash;&gt;-->
+<!--        <h4 style="color:#212529; font-family: 'Arial', sans-serif;"> {{ itemView.transactionType }}: {{-->
+<!--            itemView.itemName-->
+<!--          }}</h4>-->
+<!--        <div class="col me-2">-->
+
+
+<!--          <div class="form-floating mt-3">-->
+
+<!--            <p>-->
+<!--              {{ itemView.description }}-->
+<!--            </p>-->
+<!--&lt;!&ndash;              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"&ndash;&gt;-->
+<!--&lt;!&ndash;                        style="width: 400px; height: 150px; border: none"&ndash;&gt;-->
+<!--&lt;!&ndash;                        type="text"&ndash;&gt;-->
+<!--&lt;!&ndash;                        :value="itemEdit.description"&ndash;&gt;-->
+<!--&lt;!&ndash;                        @input="$emit('event-update-description', $event.target.value)">{{ itemView.description }}</textarea>&ndash;&gt;-->
+<!--          </div>-->
+
+
+<!--          <div class="mt-1">-->
+<!--            <div>Kogus: {{ itemView.totalQuantity }}</div>-->
+<!--          </div>-->
+<!--          <div class="mt-1">-->
+<!--            <div>Kategooria: {{ itemView.category }}</div>-->
+<!--          </div>-->
+<!--          <div class="mt-3">-->
+<!--            <div>Kasutaja: {{ itemView.username }}</div>-->
+<!--          </div>-->
+<!--          <div class="mt-1">-->
+<!--            <div>Asukoht: {{ itemView.county }}, {{ itemView.region }}</div>-->
+<!--          </div>-->
+<!--          <div class="mt-1">-->
+<!--            Staatus-->
+<!--          </div>-->
+<!--          <div class="mt-3" v-if="isOwner">-->
+<!--            <button @click="openItemInfoModal" type="button" class="btn btn-outline-success me-3">Muuda andmeid</button>-->
+
+<!--            &lt;!&ndash; @click="updateItemInfoModal" todo: andmeid saab muuta ja pilti kustutada modalis&ndash;&gt;-->
+<!--            &lt;!&ndash; <ImageInput @event-new-image-selected="$emit('event-new-image-selected', $event)"/>&ndash;&gt;-->
+
+<!--          </div>-->
+
+<!--          <div v-else>-->
+<!--            <div class="mt-3">-->
+<!--              <button type="button" class="btn btn-success me-3" @click="openMessageModal">Saada kasutajale teade-->
+<!--              </button>-->
+<!--            </div>-->
+<!--          </div>-->
+
+<!--        </div>-->
+<!--        <div class="col">-->
+<!--          <div class="mt-3">-->
+
+
+<!--            <ItemImage :image-data="itemView.itemImage" alt="Taimepilt"/>-->
+
+
+<!--          </div>-->
+
+<!--          <div class="mt-3">-->
+<!--            <button type="button" class="btn bg-secondary-subtle me-3">Broneerin</button>-->
+<!--          </div>-->
+
+<!--          <div class="mt-3">-->
+<!--            <button @click="navigateToHomeView" type="button" class="btn btn-success me-3">Avalehele</button>-->
+<!--          </div>-->
+<!--          <div>-->
+<!--            <div class="mt-3">-->
+<!--              <button type="button" class="btn btn-secondary me-3">Kustuta kuulutus</button>-->
+<!--              &lt;!&ndash;              todo: siia Modal, mis küsib kustutamise kinnitust&ndash;&gt;-->
+<!--            </div>-->
+<!--          </div>-->
+
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+<!--  </div>-->
+<!--</template>-->
 
 <script>
 
